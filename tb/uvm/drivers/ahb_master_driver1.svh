799d5cf0597dac89e04945d4e11ca92988840be7
bharatdubey <bharat1999dubey@gmail.com>
2025-12-25 10:08:39 +0530

    driver is not implementing pipelining properly, but moving ahead in interest of time

--- tb/uvm/drivers/ahb_master_driver1.svh
+++ tb/uvm/drivers/ahb_master_driver1.svh
@@ -13,13 +13,13 @@ class ahb_master_driver extends uvm_driver #(ahb_seq_item);
     extern task run_phase (uvm_phase phase);
     extern task automatic drive_control (ahb_seq_item req_current);
     extern task automatic drive_data (ahb_seq_item req_current);
-endclass : ahb_master_driver 
+endclass : ahb_master_driver
 
 function void ahb_master_driver::build_phase(uvm_phase phase);
     super.build_phase(phase);
         if (!uvm_config_db#(virtual ahb_if.master_mp)::get(this, "", "ahb_vif", dut_vif)) begin  //not checking null== dut_vif, as we are getting it here from config database
             `uvm_fatal("DRIVER:NOVIF","unable to get VIF from uvm_config_db")
-        end  
+        end
 endfunction : build_phase
 
 
@@ -35,40 +35,40 @@ task ahb_master_driver::run_phase(uvm_phase phase);
   seq_item_port.get_next_item(req_current);                    //fetching current req
 
   forever begin
-    `uvm_info("DRIVER", $sformatf("HADDR=0x%08h HSIZE=%0d HTRANS=%0b HWRITE=%0b HWDATA=0x%08h",
-            req_current.HADDR, req_current.HSIZE, req_current.HTRANS, req_current.HWRITE, req_current.HWDATA), UVM_MEDIUM)
-
     @(dut_vif.driver_cb);
     drive_control(req_current);                                 //CONTROL PHASE START AS SOON AS THERE IS A SEQ_ITEM AVAILABLE
+    `uvm_info("DRIVER", $sformatf("drive_control(current): HADDR=0x%08h HSIZE=%0d HTRANS=%0b HWRITE=%0b HWDATA=0x%08h",
+            req_current.HADDR, req_current.HSIZE, req_current.HTRANS, req_current.HWRITE, req_current.HWDATA), UVM_MEDIUM)
     if(have_next) begin
-        do @(dut_vif.driver_cb);
-        while(!dut_vif.driver_cb.HREADY);                           //HREADY WILL START DATA PHASE
+        while(!dut_vif.driver_cb.HREADY) @(dut_vif.driver_cb);                                   //HREADY WILL START DATA PHASE
         drive_data(req_next);
+        `uvm_info("DRIVER", $sformatf("drive_data(next): HADDR=0x%08h HSIZE=%0d HTRANS=%0b HWRITE=%0b HWDATA=0x%08h",
+            req_next.HADDR, req_next.HSIZE, req_next.HTRANS, req_next.HWRITE, req_current.HWDATA), UVM_MEDIUM)
         have_next = 0;
         seq_item_port.item_done(req_next);
+        `uvm_info("DRIVER", "req_next: Item done", UVM_MEDIUM)
     end
 
-    do @(dut_vif.driver_cb);
-    while(!dut_vif.driver_cb.HREADY);                           //HREADY WILL START DATA PHASE
+    do @(dut_vif.driver_cb);                                                //add one cycle delay as control phase is one cycle minimum
+    while(!dut_vif.driver_cb.HREADY);                                       //HREADY WILL START DATA PHASE
     drive_data(req_current);
+    `uvm_info("DRIVER", $sformatf("drive_data(current): HADDR=0x%08h HSIZE=%0d HTRANS=%0b HWRITE=%0b HWDATA=0x%08h",
+            req_current.HADDR, req_current.HSIZE, req_current.HTRANS, req_current.HWRITE, req_current.HWDATA), UVM_MEDIUM)
 
-    if (!have_next) begin                                       //check if there is next txn in sequence for pipelining 
-        if (seq_item_port.try_next_item(req_next)) begin        //1 if next item available
-            have_next=1;
+    seq_item_port.item_done(req_current);                                   //transaction complete
+    `uvm_info("DRIVER", "req_current: Item done", UVM_MEDIUM)
+
+    if (!have_next && seq_item_port.has_do_available()) begin               //check if there is next txn in sequence for pipelining
+        seq_item_port.try_next_item(req_next);
+        have_next = 1;
+        if (have_next) begin                                                //fetch txn for next cycle
+            //seq_item_port.get_next_item(req_next);
+            drive_control(req_next);
+            `uvm_info("DRIVER", $sformatf("drive_control(next): HADDR=0x%08h HSIZE=%0d HTRANS=%0b HWRITE=%0b HWDATA=0x%08h",
+                req_next.HADDR, req_next.HSIZE, req_next.HTRANS, req_next.HWRITE, req_current.HWDATA), UVM_MEDIUM)
         end
     end
-
-    seq_item_port.item_done(req_current);                                  //transaction complete  
-    `uvm_info("DRIVER", "Item done", UVM_MEDIUM)
-
-
-    if (have_next) begin                                        //fetch txn for next cycle
-        seq_item_port.get_next_item(req_next);
-        drive_control(req_next);
-        
-    end else begin
-        seq_item_port.get_next_item(req_current);
-    end
+    seq_item_port.get_next_item(req_current);
 
   end
 endtask : run_phase
@@ -76,7 +76,7 @@ endtask : run_phase
 
 task automatic ahb_master_driver::drive_control(ahb_seq_item req_current);
     dut_vif.driver_cb.HWRITE  <= req_current.HWRITE;
-    dut_vif.driver_cb.HTRANS  <= req_current.HTRANS; 
+    dut_vif.driver_cb.HTRANS  <= req_current.HTRANS;
     dut_vif.driver_cb.HSIZE   <= req_current.HSIZE;
     dut_vif.driver_cb.HADDR   <= req_current.HADDR;
 
@@ -84,6 +84,5 @@ endtask : drive_control
 
 task automatic ahb_master_driver::drive_data(ahb_seq_item req_current);
     if(dut_vif.driver_cb.HWRITE)
-        dut_vif.driver_cb.HWDATA  <= req_current.HWDATA;   
+        dut_vif.driver_cb.HWDATA  <= req_current.HWDATA;
 endtask : drive_data
-
