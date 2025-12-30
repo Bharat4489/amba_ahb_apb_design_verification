1821cffa5f0de1237e0c52b510e2bbf4a5472ec3
bharatdubey <bharat1999dubey@gmail.com>
2025-12-30 13:47:32 +0530

    updating split slave code

--- rtl/ahb/slaves/ahb_split_slave.sv
+++ rtl/ahb/slaves/ahb_split_slave.sv
@@ -1,44 +1,58 @@
 // Registers
 
-module ahb_split_slave (
-    ahb_if.slave_mp split_if;
+module ahb_split_slave #(parameter int SPLIT_DELAY = 5) (
+    ahb_if.slave_mp split_if
 );
+
     logic split_pending;
     int  split_cnt;
     int  blocked_master;
 
-    // Address phase: decide to split
-    if (sel && !split_pending) begin
-    split_pending  <= 1'b1;
-    split_cnt      <= 0;
-    blocked_master <= HMASTER;
+    // Address phase: capture SPLIT decision & timing
+    always_ff @(posedge HCLK or negedge HRESETn) begin
+        if (!HRESETn) begin
+            split_pending  <= 1'b0;
+            split_cnt      <= 0;
+            blocked_master <= '0;
+        end
+        else begin
+            // Address phase: decide to SPLIT
+            if (sel && !split_pending) begin
+            split_pending  <= 1'b1;
+            split_cnt      <= 0;
+            blocked_master <= HMASTER;
+            end
+            else if (split_pending) begin
+            split_cnt <= split_cnt + 1;
+            end
+        end
     end
 
-    // SPLIT response
+    // SPLIT response-drive bus response
     always_comb begin
-    HREADY = 1'b1;
-    HRESP  = OKAY;
-    HSPLIT = '0;
+        HREADY = 1'b1;
+        HRESP  = OKAY;
+        HSPLIT = '0;
 
-    if (split_pending) begin
-        HREADY = 1'b0;
-        HRESP  = SPLIT;
+        if (split_pending) begin
+            HREADY = 1'b0;
+            HRESP  = SPLIT;
+        end
     end
-    end
+
 
     // Re-enable master after delay
     always_ff @(posedge HCLK or negedge HRESETn) begin
-    if (!HRESETn) begin
-        split_pending <= 0;
-        split_cnt     <= 0;
-    end else if (split_pending) begin
-        split_cnt <= split_cnt + 1;
-
-        if (split_cnt == SPLIT_DELAY) begin
-        HSPLIT[blocked_master] <= 1'b1;
-        split_pending <= 1'b0;
+        if (!HRESETn) begin
+            HSPLIT <= '0;
+        end
+        else if (split_pending && split_cnt == SPLIT_DELAY) begin
+            HSPLIT[blocked_master] <= 1'b1;
+        end
+        else begin
+            HSPLIT <= '0;
         end
     end
-    end
+
 
 endmodule
